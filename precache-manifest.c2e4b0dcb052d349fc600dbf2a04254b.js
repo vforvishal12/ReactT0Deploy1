085340799c3433ddff6914df38ac7315fd935f2a
vforvishal12 <52970035+vforvishal12@users.noreply.github.com>
2020-04-27 20:49:09 +0530

    Add files via upload

---
+++ precache-manifest.c2e4b0dcb052d349fc600dbf2a04254b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c89822f1dd3e034459784227fb410e31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12f1f790165fa94d9ba0",
+    "url": "/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "09e670bceea17ddb34a8",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "12f1f790165fa94d9ba0",
+    "url": "/static/js/2.e96adef4.chunk.js"
+  },
+  {
+    "revision": "9e01485bbe89a62e63371375e1cfa58f",
+    "url": "/static/js/2.e96adef4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09e670bceea17ddb34a8",
+    "url": "/static/js/main.7b4b3855.chunk.js"
+  },
+  {
+    "revision": "6e4d3eeeb7b81e1437d4",
+    "url": "/static/js/runtime-main.482db4b3.js"
+  }
+]);
